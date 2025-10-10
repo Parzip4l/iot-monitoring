fff6e404a2d2cbf9829a10fffbed37499e1197e4
mochreza1407 <sobirinjamo33@gmail.com>
2025-10-10 22:34:08 +0700

    update

--- resources/views/pages/admin/mqtt/index.blade.php
+++ resources/views/pages/admin/mqtt/index.blade.php
@@ -42,7 +42,7 @@
                                 <tr>
                                     <td>{{ $i+1 }}</td>
                                     <td>{{ $mq->topic }}</td>
-                                    <td>{{ $mq->device->name }} [{{$mq->device_id}}]</td>
+                                    <td>{{ $mq->device->name }} [{{$mq->device_id}}] ?? '-'</td>
                                     <td>{{ $mq->interval}}</td>
                                     <td>
                                         <!-- Edit Button -->
